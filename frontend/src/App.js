d886a9d817a95c9b8cb564c5394333b75d942e46
Maksim Baravy <35226420+maximkatut@users.noreply.github.com>
2020-09-22 18:19:40 -0400

    implement home page with available products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
-  return <h1>hello</h1>;
+  return (
+    <>
+      <Header></Header>
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer></Footer>
+    </>
+  );
 };
 
 export default App;
